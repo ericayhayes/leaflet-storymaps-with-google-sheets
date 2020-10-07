9046c9d63d15701ce416b31b77dd8dd63596baad
Erica Y. Hayes <ericay.hayes@gmail.com>
2020-10-07 14:31:49 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GMfA8fzfSlDPET8O1_6uWvBOjC4M5-8eRiy4UlJRtrc/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1GMfA8fzfSlDPET8O1_6uWvBOjC4M5-8eRiy4UlJRtrc/';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
