3dbc4cddcc8aedf7c2028451efaa94eb0fd8c238
Erica Y. Hayes <ericay.hayes@gmail.com>
2020-10-07 12:58:42 -0400

    Revert "Merge branch 'master' into pr/1"
    This reverts commit db57d232292864a96f1bffc0f4f9e7fb92fd1b31, reversing
    changes made to 512ee8d2b2235a54532501b9b255f31ae52f014a.

--- scripts/constants.js
+++ scripts/constants.js
@@ -5,12 +5,9 @@ var constants = {
   _mapLogo: 'Storymap Logo',
   // Credits
 	_authorName: 'Author Name',
-  _webDeveloper: 'Web Development Assistant',
-  _projectWebsite: 'Project website',
 	_authorURL: 'Author Email or Website',
-  _webDeveloperURL: 'Web Development Assistant Email',
   _codeCredit: 'Code Credit',
-	_githubRepo: 'GitHub Repo',
+	_githubRepo: 'Author GitHub Repo Link',
 	// Map Settings
 	_tileProvider: 'Basemap Tiles',
 	_zoomControls: 'Zoom Controls',
